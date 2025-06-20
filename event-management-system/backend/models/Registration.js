473a3d5c13eeaccfcd741c9f1a7978436af751c7
Kabijake <kabijakep@gmail.com>
2025-06-20 04:08:41 +0000

    Modify backend

--- event-management-system/backend/models/Registration.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const registrationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
-  registeredAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Registration', registrationSchema);
